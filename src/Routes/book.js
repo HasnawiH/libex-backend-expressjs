cbb9f1476b62ca05a0e39235dc8b076f15f74ef8
HasnawiH <hasnawihaeba@gmail.com>
2019-12-05 06:08:29 -0800

    edit verify

--- src/Routes/book.js
+++ src/Routes/book.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const bookController = require("../Controllers/book");
-const auth = require("../Middleware/auth");
+// const auth = require("../Middleware/auth");
 
 router
   .get("/", bookController.getAll)
   .get("/search/:title", bookController.getByTitle)
   .get("/genre/:genre", bookController.getByGenre)
   .get("/status/:status", bookController.getByStatus)
-  .post("/add", auth.verifyAdmin, auth.verifyToken, bookController.postBook)
-  .put("/update/:id", auth.verifyAdmin, auth.verifyToken, bookController.updateBook)
-  .delete("/:id", auth.verifyAdmin, auth.verifyToken, bookController.deleteBook);
+  .post("/add", bookController.postBook)
+  .put("/update/:id", bookController.updateBook)
+  .delete("/:id", bookController.deleteBook);
 
 module.exports = router;
