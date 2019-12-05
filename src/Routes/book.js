3aa991f2f907025e91e7b95433644bc2fca1fab4
HasnawiH <hasnawihaeba@gmail.com>
2019-12-05 05:47:25 -0800

    add verifytoken

--- src/Routes/book.js
+++ src/Routes/book.js
@@ -8,8 +8,8 @@ router
   .get("/search/:title", bookController.getByTitle)
   .get("/genre/:genre", bookController.getByGenre)
   .get("/status/:status", bookController.getByStatus)
-  .post("/add", auth.verifyAdmin, bookController.postBook)
-  .put("/update/:id", auth.verifyAdmin, bookController.updateBook)
-  .delete("/:id", auth.verifyAdmin, bookController.deleteBook);
+  .post("/add", auth.verifyAdmin, auth.verifyToken, bookController.postBook)
+  .put("/update/:id", auth.verifyAdmin, auth.verifyToken, bookController.updateBook)
+  .delete("/:id", auth.verifyAdmin, auth.verifyToken, bookController.deleteBook);
 
 module.exports = router;
